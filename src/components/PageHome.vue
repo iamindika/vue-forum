345963db913c9d9e7707c6e5a9108b65c1de1eed
iamindika <ikolamba2@gmail.com>
2022-04-23 14:52:48 -0400

    create initial page-home component

---
+++ src/components/PageHome.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{thread.title}}</h2>
+
+    <div v-for="postId in thread.posts" :key="postId">
+      <h4>{{findUser(findPost(postId).userId).name}}</h4>
+      <p>{{findPost(postId).text}}</p>  
+    </div>
+  </div>
+</template>
+
+<script>
+  import sourceData from '@/data.json'
+  console.log(sourceData);
+  export default {
+    data() {
+      return {
+        threads: sourceData.threads,
+        posts: sourceData.posts,
+        users: sourceData.users
+      }
+    },
+    methods: {
+      findPost(postId) {
+        return this.posts.find((post) => post.id === postId)
+      },
+      findUser(userId) {
+        return this.users.find((user) => user.id === userId)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
