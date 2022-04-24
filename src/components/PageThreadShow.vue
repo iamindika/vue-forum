9645e4e5cb50eaf12cee00f7e1ee8a22da09eace
iamindika <ikolamba2@gmail.com>
2022-04-24 04:13:38 -0400

    copy template and script from PageHome to new PageThreadShow component
    define computed thread property and props

--- src/components/PageThreadShow.vue
+++ src/components/PageThreadShow.vue
@@ -1,9 +1,6 @@
 <template>
-  <div 
-    v-for="thread in threads" 
-    :key="thread.id"
-    class="col-large push-top"
-  >
+  <div class="col-large push-top">
+    
     <h1>{{thread.title}}</h1>
 
     <div class="post-list">
@@ -51,11 +48,22 @@
   import sourceData from '@/data.json'
   
   export default {
+    props: {
+      id: {
+        required: true,
+        type: String
+      }
+    },
     data() {
       return {
         threads: sourceData.threads,
         posts: sourceData.posts,
         users: sourceData.users
+      }
+    },
+    computed: {
+      thread() {
+        return this.threads.find((thread) => thread.id === this.id)
       }
     },
     methods: {
